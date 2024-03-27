99a1ff362f2e24ac619ac5f1bc5f7fd5b9ff2bb3
nazarfedets <160725999+nazarfedets@users.noreply.github.com>
2024-03-27 12:36:36 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+    int n=70;
+
+    int low=50;
+    int upper=100;
+    if(n > low & n < upper)
+        System.out.println("Число "+n+" міститься в проміжку("+low+";"+upper+")");
+    else
+        System.out.println("Число "+n+" не міститься в проміжку("+low+";"+upper+")");
+
+
+
+
+
+
+    }
+}
